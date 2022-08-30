ab83799913fa71a10d5e2e240d8453d128a8b988
Phuong Bui <buiphuongaptutc@gmail.com>
2022-08-30 18:05:02 +0700

    split up to modules

---
+++ starter/catpoint-parent/security/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module security {
+    requires image;
+    requires java.desktop;
+    requires miglayout;
+    requires com.google.common;
+    requires java.prefs;
+    requires com.google.gson;
+}
